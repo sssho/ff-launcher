f00d0acc7154484049299c3e78bdce876595bbb3
sssho <43515414+sssho@users.noreply.github.com>
2022-08-18 01:33:14 +0900

    Use my shortcut parser instead of go-ole

--- lib/shortcut.go
+++ lib/shortcut.go
@@ -1,16 +1,19 @@
 package lib
 
 import (
+	"bufio"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"io/fs"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"time"
 
-	"github.com/go-ole/go-ole"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 type Shortcut struct {
@@ -33,8 +36,141 @@ func NewShortcut(dir string, finfo fs.FileInfo, org Origin) (s *Shortcut, err er
 	return &shortcut, nil
 }
 
-func (s *Shortcut) Resolve(w *WscriptShell) error {
-	tpath, args, err := ResolveShortcut(s.Path, w)
+const LINKFLAGS_OFFSET = 20
+const HEADERSIZE = 0x4c
+
+type LinkFlags struct {
+	HasLinkTargetIDList         bool
+	HasLinkInfo                 bool
+	HasName                     bool
+	HasRelativePath             bool
+	HasWorkingDir               bool
+	HasArguments                bool
+	HasIconLocation             bool
+	IsUnicode                   bool
+	ForceNoLinkInfo             bool
+	HasExpString                bool
+	RunInSeparateProcess        bool
+	Unused1                     bool
+	HasDarwinID                 bool
+	RunAsUser                   bool
+	HasExpIcon                  bool
+	NoPidlAlias                 bool
+	Unused2                     bool
+	RunWithShimLayer            bool
+	ForceNoLinkTrack            bool
+	EnableTargetMetadata        bool
+	DisableLinkPathTracking     bool
+	DisableKnownFolderTracking  bool
+	DisableKnownFolderAlias     bool
+	AllowLinkToLink             bool
+	UnaliasOnSave               bool
+	PreferEnvironmentPath       bool
+	KeepLocalIDListForUNCTarget bool
+}
+
+func NewLinkFlags(flag uint32) LinkFlags {
+	var l LinkFlags
+	l.HasLinkTargetIDList = ((flag >> 0) & 1) == 1
+	l.HasLinkInfo = ((flag >> 1) & 1) == 1
+	l.HasName = ((flag >> 2) & 1) == 1
+	l.HasRelativePath = ((flag >> 3) & 1) == 1
+	l.HasWorkingDir = ((flag >> 4) & 1) == 1
+	l.HasArguments = ((flag >> 5) & 1) == 1
+	l.HasIconLocation = ((flag >> 6) & 1) == 1
+	l.IsUnicode = ((flag >> 7) & 1) == 1
+	l.ForceNoLinkInfo = ((flag >> 8) & 1) == 1
+	l.HasExpString = ((flag >> 9) & 1) == 1
+	l.RunInSeparateProcess = ((flag >> 10) & 1) == 1
+	l.Unused1 = ((flag >> 11) & 1) == 1
+	l.HasDarwinID = ((flag >> 12) & 1) == 1
+	l.RunAsUser = ((flag >> 13) & 1) == 1
+	l.HasExpIcon = ((flag >> 14) & 1) == 1
+	l.NoPidlAlias = ((flag >> 15) & 1) == 1
+	l.Unused2 = ((flag >> 16) & 1) == 1
+	l.RunWithShimLayer = ((flag >> 17) & 1) == 1
+	l.ForceNoLinkTrack = ((flag >> 18) & 1) == 1
+	l.EnableTargetMetadata = ((flag >> 19) & 1) == 1
+	l.DisableLinkPathTracking = ((flag >> 20) & 1) == 1
+	l.DisableKnownFolderTracking = ((flag >> 21) & 1) == 1
+	l.DisableKnownFolderAlias = ((flag >> 22) & 1) == 1
+	l.AllowLinkToLink = ((flag >> 23) & 1) == 1
+	l.UnaliasOnSave = ((flag >> 24) & 1) == 1
+	l.PreferEnvironmentPath = ((flag >> 25) & 1) == 1
+	l.KeepLocalIDListForUNCTarget = ((flag >> 26) & 1) == 1
+	return l
+}
+
+func ResolveShortcut(file *os.File) (path string, err error) {
+	_, err = file.Seek(LINKFLAGS_OFFSET, io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+	var LinkFlags uint32
+	err = binary.Read(file, binary.LittleEndian, &(LinkFlags))
+	if err != nil {
+		return "", err
+	}
+	lf := NewLinkFlags(LinkFlags)
+	if !lf.HasLinkInfo {
+		return "", fmt.Errorf("linkinfo not found")
+	}
+	var LinkTargetIDListSize uint16
+	if lf.HasLinkTargetIDList {
+		_, err = file.Seek(HEADERSIZE, io.SeekStart)
+		if err != nil {
+			return "", err
+		}
+		err = binary.Read(file, binary.LittleEndian, &(LinkTargetIDListSize))
+		if err != nil {
+			return "", err
+		}
+	}
+	var LinkInfoStartAddr int64 = int64(HEADERSIZE + 2 + LinkTargetIDListSize)
+	_, err = file.Seek(LinkInfoStartAddr+8, io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+	var LinkInfoFlags uint32
+	err = binary.Read(file, binary.LittleEndian, &(LinkInfoFlags))
+	if err != nil {
+		return "", err
+	}
+	VolumeIDAndLocalBasePath := (LinkInfoFlags & 1) == 1
+	if !VolumeIDAndLocalBasePath {
+		return "", fmt.Errorf("localbasepath not found")
+	}
+	var LocalBasePathOffset uint32
+	_, err = file.Seek(LinkInfoStartAddr+16, io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+	err = binary.Read(file, binary.LittleEndian, &(LocalBasePathOffset))
+	if err != nil {
+		return "", err
+	}
+	var LocalBasePathAddr int64 = int64(LinkInfoStartAddr + int64(LocalBasePathOffset))
+	_, err = file.Seek(LocalBasePathAddr, io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+	// https://zenn.dev/mattn/articles/fd545a14b0ffdf
+	jr := transform.NewReader(file, japanese.ShiftJIS.NewDecoder())
+	br := bufio.NewReader(jr)
+	path, err = br.ReadString(0)
+	if err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func (s *Shortcut) Resolve() error {
+	f, err := os.Open(s.Path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	tpath, err := ResolveShortcut(f)
 	if err != nil {
 		return err
 	}
@@ -55,7 +191,7 @@ func (s *Shortcut) Resolve(w *WscriptShell) error {
 		parent = filepath.Dir(tpath)
 	}
 	s.TPath = tpath
-	s.Args = args
+	s.Args = "" // TODO
 	s.IsDir = isdir
 	s.Parent = parent
 
@@ -72,65 +208,21 @@ func (s Shortcut) Text() (text string) {
 	return
 }
 
-func worker(input chan fs.FileInfo, output chan Shortcut, dir string, org Origin, w *WscriptShell) {
-	for finfo := range input {
-		shortcut, err := NewShortcut(dir, finfo, org)
-		if err != nil {
-			output <- *shortcut
-			continue
-		}
-		_ = shortcut.Resolve(w)
-		output <- *shortcut
-	}
-}
-
 func NewShortcutList(dir string, origin Origin) ([]Shortcut, error) {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return nil, err
 	}
-
-	workNum := len(files)
-	input := make(chan fs.FileInfo, workNum)
-	output := make(chan Shortcut)
-
-	for _, f := range files {
-		input <- f
-	}
-
-	shortcuts := make([]Shortcut, 0, workNum)
-
-	ole.CoInitializeEx(0, ole.COINIT_MULTITHREADED|ole.COINIT_DISABLE_OLE1DDE|ole.COINIT_SPEED_OVER_MEMORY)
-	defer ole.CoUninitialize()
-	w, err := NewWscriptShell()
-	if err != nil {
-		return nil, err
-	}
-	defer w.Release()
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go worker(input, output, dir, origin, w)
-	}
-	// close(input)
-	end := make(chan bool)
-	go func() {
-		var done int
-		for v := range output {
-			done++
-			if v.TPath == "" {
-				if done == workNum {
-					break
-				}
-				continue
-			}
-			shortcuts = append(shortcuts, v)
-			if done == workNum {
-				break
-			}
+	shortcuts := make([]Shortcut, 0, len(files))
+	for _, file := range files {
+		shortcut, _ := NewShortcut(dir, file, origin)
+		err = shortcut.Resolve()
+		if err != nil {
+			continue
 		}
-		end <- true
-		// close(output)
-	}()
-	<-end
+		shortcuts = append(shortcuts, *shortcut)
+	}
+
 	return shortcuts, nil
 }
 
